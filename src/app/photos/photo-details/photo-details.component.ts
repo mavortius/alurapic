ad9e504ab83dce15602e8f3e607c423ed67947ed
Marcelo Martins <m.a.martins@live.com>
2021-10-27 16:48:19 -0300

    Extraindo parâmetros de rotas

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params.photoId;
+
   }
 
 }

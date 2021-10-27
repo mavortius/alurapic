50b1e7761f733cc08e6259afe4574ea5d03c479f
Marcelo Martins <m.a.martins@live.com>
2021-10-27 16:35:51 -0300

    Bloqueando acesso não autenticado

--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -1,34 +1,35 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home.component';
-import { AuthGuard } from '../core/auth/auth.guard';
+import { LoginGuard } from '../core/auth/login.guard';
 import { SignInComponent } from './signin/signin.component';
 import { SignUpComponent } from './singup/singup.component';
 
 const routes: Routes = [
-    { 
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [LoginGuard],
+    children: [
+      {
         path: '',
-        component: HomeComponent,
-        canActivate: [AuthGuard],
-        children: [
-            { 
-                path: '',
-                component: SignInComponent,
-            }, 
-            { 
-                path: 'signup',
-                component: SignUpComponent,
-            },            
-        ]
-    },              
+        component: SignInComponent,
+      },
+      {
+        path: 'signup',
+        component: SignUpComponent,
+      },
+    ]
+  },
 ];
 
 @NgModule({
-    imports: [ 
-        RouterModule.forChild(routes) 
-    ],
-    exports: [ RouterModule ]
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {
+}
 

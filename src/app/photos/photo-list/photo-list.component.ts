8abcd39944a71409c7d9f942a564493717868678
Marcelo Martins <m.a.martins@live.com>
2021-10-27 17:40:40 -0300

    Correção de parâmetro de rota

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
+import { PhotoService } from '../photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -12,19 +12,21 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
-  filter: string = '';
-  hasMore: boolean = true;
-  currentPage: number = 1;
-  userName: string = '';
+  filter = '';
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private photoService: PhotoService
-  ) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private photoService: PhotoService) {
+  }
 
   ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params.userName;
-    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.activatedRoute.params
+      .subscribe((params) => {
+        this.userName = params.userName;
+        this.photos = this.activatedRoute.snapshot.data['photos'];
+      });
   }
 
   load() {
@@ -33,7 +35,9 @@ export class PhotoListComponent implements OnInit {
       .subscribe(photos => {
         this.filter = '';
         this.photos = this.photos.concat(photos);
-        if(!photos.length) this.hasMore = false;
+        if (!photos.length) {
+          this.hasMore = false;
+        }
       });
   }
 }

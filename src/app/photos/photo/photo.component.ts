83d56ef2b054407eb6746b4630a94d6e37bc44bb
Marcelo Martins <m.a.martins@live.com>
2021-10-27 15:49:51 -0300

    Adequando a exibição das fotos

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,12 +1,26 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD_URL = 'http://localhost:3000/imgs/';
+
 @Component({
-    selector: 'ap-photo',
-    templateUrl: 'photo.component.html'
+  // tslint:disable-next-line:component-selector
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-    
-    @Input() description='';
-    
-    @Input() url='';
+  private _url = '';
+
+  @Input() description = '';
+
+  @Input() set url(url: string) {
+    if (!url.startsWith('data')) {
+      this._url = CLOUD_URL + url;
+    } else {
+      this._url = url;
+    }
+  }
+
+  get url() {
+    return this._url;
+  }
 }

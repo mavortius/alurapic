7b5a7dc0666db6a503e80e6ff7e8727790ddc4c6
Marcelo Martins <m.a.martins@live.com>
2021-10-27 16:42:10 -0300

    Rotas parametrizadas

---
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'ap-photo-details',
+  templateUrl: './photo-details.component.html'
+})
+export class PhotoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
